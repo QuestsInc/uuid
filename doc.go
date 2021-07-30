e9d7df4c956c21e2a4dc0f1be06d9a8bf1f267f8
yuriimakohon <yuriimakohon@gmail.com>
2021-07-30 16:35:55 +0300

    Add doc.go

---
+++ doc.go
@@ -0,0 +1,32 @@
+// Package uuid is satori/go.uuid rework with some changes.
+//
+// Package provides UUID v4 generation and utils for compare, zero check, parsing, converting etc.
+//
+// Exportable functions:
+//
+// New
+//
+// Equal
+//
+// FromBytes
+//
+// FromBytesOpt
+//
+// FromBytes
+//
+// FromBytesOpt
+//
+// Exportable methods of UUID:
+//
+// UUID.String
+//
+// UUID.IsZero
+//
+// UUID.MarshalJSON
+//
+// UUID.UnmarshalJSON
+//
+// UUID.Value
+//
+// UUID.Scan
+package uuid
